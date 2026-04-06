147f9c376b4b74bf77301d657d2daa97e4134afd
Chay23 <scherbiynazar@gmail.com>
2026-04-06 21:06:20 +0300

    Add shape deletion via context menu

---
+++ src/components/molecules/rectangle/rectangle-context-menu-content.tsx
@@ -0,0 +1,41 @@
+import {
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+} from '@/components/atoms/dropdown-menu';
+import { useSelectedShape } from '@/store/shapes/selectors';
+import { useShapes } from '@/store/shapes/shapes';
+
+export default function RectangleContextMenuContent() {
+  const selectedShape = useSelectedShape();
+  const deleteShape = useShapes((state) => state.deleteShape);
+
+  const handleShapeDelete = () => {
+    if (selectedShape) {
+      deleteShape(selectedShape.id);
+    }
+  };
+
+  return (
+    <DropdownMenuGroup>
+      <DropdownMenuItem>
+        Rotate 90°
+        <DropdownMenuShortcut>Ctrl + R</DropdownMenuShortcut>
+      </DropdownMenuItem>
+      <DropdownMenuItem>
+        Bring to front
+        <DropdownMenuShortcut>{`Ctrl + ]`}</DropdownMenuShortcut>
+      </DropdownMenuItem>
+      <DropdownMenuItem>
+        Send to back
+        <DropdownMenuShortcut>{`Ctrl + [`}</DropdownMenuShortcut>
+      </DropdownMenuItem>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem onPointerUp={handleShapeDelete}>
+        Delete
+        <DropdownMenuShortcut>Delete</DropdownMenuShortcut>
+      </DropdownMenuItem>
+    </DropdownMenuGroup>
+  );
+}
--- src/components/molecules/shape-context-menu-content.tsx
+++ src/components/molecules/shape-context-menu-content.tsx
@@ -1,43 +1,15 @@
-import {
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-} from '@/components/atoms/dropdown-menu';
 import { TYPE_RECTANGLE } from '@/lib/constants/common';
 import { useSelectedShape } from '@/store/shapes/selectors';
-import type { s } from '@/types';
+import ReactangleContextMenuContent from './rectangle/rectangle-context-menu-content';
 
 export default function ShapeContextMenuContent() {
-  const selectedShape = useSelectedShape() as s.Shapes;
+  const selectedShape = useSelectedShape();
 
   if (!selectedShape) return null;
 
   switch (selectedShape.type) {
     case TYPE_RECTANGLE: {
-      return (
-        <>
-          <DropdownMenuGroup>
-            <DropdownMenuItem>
-              Rotate 90°
-              <DropdownMenuShortcut>Ctrl + R</DropdownMenuShortcut>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              Bring to front
-              <DropdownMenuShortcut>{`Ctrl + ]`}</DropdownMenuShortcut>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              Send to back
-              <DropdownMenuShortcut>{`Ctrl + [`}</DropdownMenuShortcut>
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              Delete
-              <DropdownMenuShortcut>Delete</DropdownMenuShortcut>
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-        </>
-      );
+      return <ReactangleContextMenuContent />;
     }
     default:
       return null;
