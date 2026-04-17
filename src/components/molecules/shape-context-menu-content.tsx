e44e0f0a6688013f3d4f1520a48fd1a268e89b1f
Chay23 <scherbiynazar@gmail.com>
2026-04-17 21:04:34 +0300

    Add context menu for ellipse

--- src/components/molecules/shape-context-menu-content.tsx
+++ src/components/molecules/shape-context-menu-content.tsx
@@ -1,6 +1,7 @@
-import { TYPE_RECTANGLE } from '@/lib/constants/common';
+import { TYPE_ELLIPSE, TYPE_RECTANGLE } from '@/lib/constants/common';
 import { useSelectedShape } from '@/store/shapes/selectors';
 import ReactangleContextMenuContent from './rectangle/rectangle-context-menu-content';
+import EllipseContextMenuContent from './ellipse/ellipse-context-menu-content';
 
 export default function ShapeContextMenuContent() {
   const selectedShape = useSelectedShape();
@@ -10,6 +11,9 @@ export default function ShapeContextMenuContent() {
   switch (selectedShape.type) {
     case TYPE_RECTANGLE: {
       return <ReactangleContextMenuContent />;
+    }
+    case TYPE_ELLIPSE: {
+      return <EllipseContextMenuContent />;
     }
     default:
       return null;
